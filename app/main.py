c1a1d59390e7a2c3ffeb4621187d48cd85d5ff2d
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-01-14 11:22:13 -0500

    modify converter.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
-
+from app.schemas import ConvertRequest, ConvertResponse
+from app.services.converter import convert_times
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+@app.post("/convert", response_model=ConvertResponse)
+def convert(req: ConvertRequest):
+    results = convert_times(req.from_timezone, req.to_timezone, req.times)
+    return {"results" : results}
--- app/services/converter.py
+++ app/services/converter.py
@@ -1,15 +1,35 @@
 from typing import List, Dict
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
-def convert_times(from_tz: str, to_tz: str, time: List[str]) -> List[Dict]:
+def convert_times(from_tz: str, to_tz: str, times: List[str]) -> List[Dict]:
     """
     Temporary implementation (dummy).
     This will be replaced with real timezone conversion logic.
     """
     results = []
-    for t in time:
-        results.append({
-            "input": t,
-            "output": f"[{from_tz} -> {to_tz}] {t}",
-            "status": "ok"
-        })
+    for t in times:
+        try:
+            dt_naive = datetime.strptime(t, "%m/%d %H:%M")
+            dt_naive = dt_naive.replace(year=datetime.now().year)
+
+            dt_from = dt_naive.replace(tzinfo=ZoneInfo(from_tz))
+
+            dt_to = dt_from.astimezone(ZoneInfo(to_tz))
+
+            output = dt_to.strftime("%m/%d (%a) %H:%M")
+
+            results.append({
+                "input": t,
+                "output": output,
+                "status": "ok"
+            })
+        
+        except Exception as e:
+            results.append({
+                "input": t,
+                "status": "error",
+                "error": str(e)
+            })
+
     return results
